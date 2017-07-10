73823f2d003b6ece23f9f22c113e83cd4462927c
Elena Grahovac <elena@grahovac.me>
2017-07-10 08:07:00 +0700

    Try on Heroku

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+func main() {
+	port := os.Getenv("PORT") // 80
+
+	redirectTo := os.Getenv("TO")     // https://google.com
+	redirectType := os.Getenv("TYPE") // 301
+
+	redirectCode, err := strconv.Atoi(redirectType)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Check of type is 301 or 302
+	if redirectCode != http.StatusPermanentRedirect &&
+		redirectCode != http.StatusFound &&
+		redirectCode != http.StatusSeeOther {
+		err := fmt.Errorf("Forwarding type %s is incorrect", redirectType)
+		log.Fatal(err)
+	}
+
+	log.Printf("Redirect to %s with type %d", redirectTo, redirectCode)
+
+	http.HandleFunc(
+		"/",
+		func(w http.ResponseWriter, r *http.Request) {
+			http.Redirect(w, r, redirectTo, redirectCode)
+		},
+	)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
